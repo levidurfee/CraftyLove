e337fd6cbb8b4ac71268d973421bac4d03488ceb
Levi Durfee <levi.durfee@gmail.com>
2016-03-06 13:32:45 -0500

    working on #3

--- craftylove/CraftyLovePlugin.php
+++ craftylove/CraftyLovePlugin.php
@@ -1,23 +1,47 @@
 <?php
 namespace Craft;
 
+/**
+ * Basics of the plugin
+ *
+ * Returns name, version, developer, developerUrl, and releaseFeedUrl
+ *
+ * @author Levi Durfee <junk@ween.io>
+ * @copyright Copyright (c) 2016, Levi Durfee
+ * @license https://raw.githubusercontent.com/levidurfee/CraftyLove/master/LICENSE
+ * @link https://6c657669.info
+ * @package craft.crafylove.plugin
+ * @since 1.0.0
+ */
 class CraftyLovePlugin extends BasePlugin
 {
+    /**
+    * @return string
+    */
     public function getName()
     {
         return 'Crafty Love';
     }
 
+    /**
+    * @return string
+    */
     public function getVersion()
     {
         return '1.0.0';
     }
 
+    /**
+    * @return string
+    */
     public function getDeveloper()
     {
         return 'Levi Durfee';
     }
 
+    /**
+    * @return string
+    */
     public function getDeveloperUrl()
     {
         return 'https://6c657669.info';
