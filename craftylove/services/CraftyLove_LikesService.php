fcce2e918a51567af77620f7ebc9ab92c413e841
Levi Durfee <levi.durfee@gmail.com>
2016-03-06 13:48:43 -0500

    Added comments/docblocks. Closes #3

--- craftylove/services/CraftyLove_LikesService.php
+++ craftylove/services/CraftyLove_LikesService.php
@@ -1,6 +1,18 @@
 <?php
 namespace Craft;
 
+/**
+* The backbone of the plugin. The service.
+*
+* add likes and get current likes for a unique id
+*
+* @author Levi Durfee <junk@ween.io>
+* @copyright Copyright (c) 2016, Levi Durfee
+* @license https://raw.githubusercontent.com/levidurfee/CraftyLove/master/LICENSE
+* @link https://6c657669.info
+* @package craft.crafylove.plugin
+* @since 1.0.0
+*/
 class CraftyLove_LikesService extends BaseApplicationComponent
 {
     protected $q;
@@ -10,14 +22,35 @@ class CraftyLove_LikesService extends BaseApplicationComponent
         $this->q = craft()->db->createCommand();
     }
 
+    /**
+    * Add a like
+    *
+    * First need to get current total likes, if any exist. Then +1 to add another
+    * like.
+    *
+    * @param int
+    *
+    * @return null
+    */
     public function addLike($id)
     {
         // get current likes
         $likes = $this->getLikesForEntry($id);
+        // +1 the likes!
         $totalLikes = $likes + 1;
+        // Run query insert or update existing record if id exists
         $this->q->insertOrUpdate('craftylove_likes', array('entryId' => $id), array('totalLikes' => $totalLikes));
     }
 
+    /**
+    * Get the current likes for an id
+    *
+    * Run a query to get the current likes for a unique id, if any.
+    *
+    * @param int
+    *
+    * @return int
+    */
     public function getLikesForEntry($id)
     {
         if(is_array($id)) {
